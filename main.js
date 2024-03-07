3f6aa62d6312ca901f5ea9c35e6f672376a230d8
Tariqkhan61 <161859910+Tariqkhan61@users.noreply.github.com>
2024-03-07 17:40:28 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,140 @@
+//BID DATA using let-var-const-comment-string-number-boolean
+//PERSONAL INFORMATION
+//Dedicated to Respected Sir Okasha 
+var fName = "Muhammad";
+var mName = "Tariq";
+var lName = "Mahboob";
+console.log(fName + " " + mName + " " + lName);
+var fatherfName = "Mahboob";
+var fathermName = "Nusrat";
+var fatherlName = "Khan";
+console.log(fatherfName + " " + fathermName + " " + fatherlName);
+var age = 61;
+console.log(age);
+var eMail = "shahanakhan246@gmail.com";
+console.log(eMail);
+var phone = 923202009631;
+console.log(phone);
+var religion = "Islam";
+console.log(religion);
+var nationality = "Pakistani";
+console.log(nationality);
+var isStudent = true;
+console.log(isStudent);
+var isEmployed = false;
+console.log(isEmployed);
+//ADDRESS
+var ADDRESS = "H.# R-1650 ABV Housing Scheme,Sector 10,";
+console.log(ADDRESS);
+var area = "Scheme 33,Ahsanabad";
+console.log(area);
+var city = "Karachi";
+console.log(city);
+//EDUCATION
+var matriculation = "Government Boys Secondary School,Shah Faisal Colony, Karachi";
+console.log(matriculation);
+var faculty = "Science";
+console.log(faculty);
+var isclear = true;
+console.log(isclear);
+var intermidiate = "Allama Iqbal Government College Air Port, Karachi";
+console.log(intermidiate);
+var faculty1 = "Science";
+console.log(faculty1);
+var isclear1 = true;
+console.log(isclear1);
+var graduation = "Jamia Millia Government College Malir, Karachi";
+console.log(graduation);
+var faculty2 = "BSc";
+console.log(faculty2);
+var isclear2 = true;
+console.log(isclear2);
+//TECHNICAL QUALIFICATION
+var apprenticeship = "Aircraft and Ground Instrumentation form Base Technical Squadron,PAF Base Faisal, Karachi";
+console.log(apprenticeship);
+var course = "Basic Aircraft and Groung Instrumentation From (BTS) No 102 AED,PAF Faisal";
+console.log(course);
+var isclear3 = true;
+console.log(isclear3);
+var course2 = " Aircraft Advance Instrumentation From (BTS) No 102 AED,PAF Faisal";
+console.log(course2);
+var isclear4 = true;
+console.log(isclear4);
+//EXPERIENCE
+var EXPERIENCE = "18 years experience of Maintenance,Overhauling and Calibration of F-16,C-130,MI171 and Mashak air craft's Instruments";
+console.log(EXPERIENCE);
+var activity = "Quality Control and assurance activities,outgoing/Inhand Repair Capability activities ";
+console.log(activity);
+//SYSTEM USED
+var testequipment = "TTU-205D,E Test system (F-16) to calibrate ALTIMETERS,AMI and VVI,Pressure Gauges";
+console.log(testequipment);
+var testers = "Monometer, Vaccum chamber, Barometer, Dead weight Tester";
+console.log(testers);
+// COMPUTER KNOWLEDGE
+var skill = "MS-Office, Html, Css (beginner level)  ";
+console.log(skill);
+var learninginprogress = "TypeScript, Artificial Intelligence, Metaverse, Blockchain, Python";
+console.log(learninginprogress);
+//HOBBIES
+var reading = "Islamic Books, Quran and Science, ";
+console.log(reading);
+var listen = "Tilawat of Abdul Rahman As Sudais, and Sheikh As Shuraim";
+console.log(listen);
+//MARITAL STATUS
+var isMarried = true;
+console.log(isMarried);
+var children = "Daughter";
+console.log(children);
+var daughtereducation = "Completed BBA(HR) in 2024 from Iqra University";
+console.log(daughtereducation);
+//FAVOURITE GAME
+var game = "Cricket";
+console.log(game);
+var game1 = "Football";
+console.log(game1);
+var favouriteplayer = "Cristinio Ronaldo (dos Santos Aveiro)";
+console.log(favouriteplayer);
+// FOOD FUSION
+var preferencefoods = "Beef Quorma with Garlic Nan, Fried Rice with Shashlic Home made";
+console.log(preferencefoods);
+var breakfast = true;
+console.log(breakfast);
+var restaurent = "Urban Tarka and Lal Qila";
+console.log(restaurent);
+//FAVOURITE DRESS
+var best_loveddress = "Jeans and T-Shirt";
+console.log(best_loveddress);
+// NAME OF UNIVERSITY WHERE YOU ARE STDYING
+var University = "The Governor Sind Kamran Tessori Harbour University of Informaion Technology";
+console.log(University);
+// SCOPE
+var scope = "To become a web developer";
+console.log(scope);
+//OWN A BIKE
+var biketype = "Honda";
+console.log(biketype);
+var bikecc = 70;
+console.log(bikecc);
+var iscolorblack = true;
+console.log(iscolorblack);
+var model = 1982;
+console.log(model);
+//PHYSICAL STATUS
+var heightinfeet = 5.9;
+console.log("height:".concat(heightinfeet, "feet"));
+var weightInKilograms = 70;
+console.log("Weight: ".concat(weightInKilograms, " kg"));
+var built = "Normal";
+console.log(built);
+var abnormality = false;
+console.log(abnormality);
+// DESIRES
+var visitto = "to Perform Hajj and Umra with wife and daughter";
+console.log(visitto);
+var visitto1 = "USA, CANADA and TURKIYA";
+console.log(visitto1);
+//FAMILY DATA
+var isdaughter = true;
+console.log(isdaughter);
+var isson = false;
+console.log(isson);
---
+++ main.ts
@@ -0,0 +1,170 @@
+//BID DATA using let-var-const-comment-string-number-boolean
+//PERSONAL INFORMATION
+//Dedicated to Respected Sir Okasha 
+
+let fName:string="Muhammad";
+let mName:string="Tariq";
+let lName:string="Mahboob";
+console.log(fName+" "+mName+" "+lName);
+
+let fatherfName:string="Mahboob";
+let fathermName:string="Nusrat";
+let fatherlName:string="Khan";
+console.log(fatherfName+" "+fathermName+" "+fatherlName);
+
+let age:number=61;
+console.log(age);
+const eMail:string="shahanakhan246@gmail.com";
+console.log(eMail);
+
+const phone:number=923202009631;
+console.log(phone);
+
+const religion:string="Islam";
+console.log(religion);
+
+const nationality:string="Pakistani";
+console.log(nationality);
+
+let isStudent:boolean=true;
+console.log(isStudent);
+
+let isEmployed:boolean=false;
+console.log(isEmployed);
+
+//ADDRESS
+var ADDRESS:string="H.# R-1650 ABV Housing Scheme,Sector 10,";
+console.log(ADDRESS);
+var area:string="Scheme 33,Ahsanabad";
+console.log(area);
+var city:string="Karachi";
+console.log(city);
+
+//EDUCATION
+const matriculation:string="Government Boys Secondary School,Shah Faisal Colony, Karachi";
+console.log(matriculation);
+const faculty:string="Science";
+console.log(faculty);
+let isclear:boolean=true
+console.log(isclear);
+const intermidiate:string="Allama Iqbal Government College Air Port, Karachi";
+console.log(intermidiate);
+const faculty1:string="Science";
+console.log(faculty1);
+let isclear1:boolean=true;
+console.log(isclear1);
+const graduation:string="Jamia Millia Government College Malir, Karachi";
+console.log(graduation);
+const faculty2:string="BSc";
+console.log(faculty2);
+let isclear2:boolean=true;
+console.log(isclear2);
+
+//TECHNICAL QUALIFICATION
+let apprenticeship:string="Aircraft and Ground Instrumentation form Base Technical Squadron,PAF Base Faisal, Karachi";
+console.log(apprenticeship);
+let course:string="Basic Aircraft and Groung Instrumentation From (BTS) No 102 AED,PAF Faisal";
+console.log(course);
+let isclear3:boolean=true;
+console.log(isclear3);
+let course2:string=" Aircraft Advance Instrumentation From (BTS) No 102 AED,PAF Faisal";
+console.log(course2);
+let isclear4:boolean=true;
+console.log(isclear4);
+
+//EXPERIENCE
+let EXPERIENCE:string="18 years experience of Maintenance,Overhauling and Calibration of F-16,C-130,MI171 and Mashak air craft's Instruments";
+console.log(EXPERIENCE);
+let activity:string="Quality Control and assurance activities,outgoing/Inhand Repair Capability activities "
+console.log(activity);
+
+//SYSTEM USED
+let testequipment:string="TTU-205D,E Test system (F-16) to calibrate ALTIMETERS,AMI and VVI,Pressure Gauges";
+console.log(testequipment);
+let testers:string="Monometer, Vaccum chamber, Barometer, Dead weight Tester"
+console.log(testers);
+
+// COMPUTER KNOWLEDGE
+var skill:string="MS-Office, Html, Css (beginner level)  ";
+console.log(skill);
+var learninginprogress:string="TypeScript, Artificial Intelligence, Metaverse, Blockchain, Python";
+console.log(learninginprogress);
+
+//HOBBIES
+let reading:string="Islamic Books, Quran and Science, ";
+console.log(reading);
+let listen:string="Tilawat of Abdul Rahman As Sudais, and Sheikh As Shuraim"
+console.log(listen);
+
+//MARITAL STATUS
+let isMarried:boolean=true;
+console.log(isMarried);
+let children:string="Daughter"
+console.log(children);
+let daughtereducation:string="Completed BBA(HR) in 2024 from Iqra University";
+console.log(daughtereducation);
+
+//FAVOURITE GAME
+let game:string="Cricket";
+console.log(game);
+let game1:string="Football";
+console.log(game1);
+let favouriteplayer:string="Cristinio Ronaldo (dos Santos Aveiro)";
+console.log(favouriteplayer);
+
+// FOOD FUSION
+let preferencefoods:string="Beef Quorma with Garlic Nan, Fried Rice with Shashlic Home made";
+console.log(preferencefoods);
+let breakfast:boolean=true;
+console.log(breakfast);
+let restaurent:string="Urban Tarka and Lal Qila";
+console.log(restaurent);
+
+//FAVOURITE DRESS
+let best_loveddress:string="Jeans and T-Shirt";
+console.log(best_loveddress);
+
+// NAME OF UNIVERSITY WHERE YOU ARE STDYING
+let University:string="The Governor Sind Kamran Tessori Harbour University of Informaion Technology"
+console.log(University);
+
+// SCOPE
+let scope:string="To become a web developer";
+console.log(scope);
+
+//OWN A BIKE
+const biketype:string="Honda";
+console.log(biketype);
+const bikecc:number=70;
+console.log(bikecc);
+const iscolorblack:boolean=true;
+console.log(iscolorblack);
+const model:number=1982;
+console.log(model);
+
+//PHYSICAL STATUS
+const heightinfeet:number=5.9;
+console.log(`height:${heightinfeet}feet`);
+const weightInKilograms: number = 70;
+console.log(`Weight: ${weightInKilograms} kg`);
+const built:string="Normal";
+console.log(built);
+const abnormality:boolean=false;
+console.log(abnormality);
+
+// DESIRES
+let visitto:string="to Perform Hajj and Umra with wife and daughter";
+console.log(visitto);
+let visitto1:string="USA, CANADA and TURKIYA";
+console.log(visitto1);
+
+//FAMILY DATA
+const isdaughter:boolean=true;
+console.log(isdaughter);
+const isson:boolean=false;
+console.log(isson);
+
+
+
+
+
